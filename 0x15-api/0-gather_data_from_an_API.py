6282db3cb3f4d043e2e35994362807c69089707b
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-24 22:36:40 +0300

    A script that displays user and completion rate

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,10 +5,11 @@ import sys
 
 
 if __name__ == "__main__":
+    """Defining an api for retrieving employee task completion rate"""
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}:".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos))
